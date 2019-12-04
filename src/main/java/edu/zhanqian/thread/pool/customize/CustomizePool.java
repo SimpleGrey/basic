2f20b521ac5d6192faa0d2916255f713fb350176
zhanqian <zhanqian@xinhuamm.net>
2019-12-04 19:57:55 +0800

    [新增] 自定义

---
+++ src/main/java/edu/zhanqian/thread/pool/customize/CustomizePool.java
@@ -0,0 +1,35 @@
+package edu.zhanqian.thread.pool.customize;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @Date 2019/12/4 19:49
+ * @Created by zhanqian
+ * @Description 自定义线程池，可以用ThreadPoolExecutor类创建，它有多个构造方法来创建线程池。
+ */
+public class CustomizePool {
+
+    public static void main(String[] args) {
+        // 创建数组型缓冲等待队列
+        BlockingQueue<Runnable> bq = new ArrayBlockingQueue<Runnable>(10);
+
+        // ThreadPoolExecutor:创建自定义线程池，池中保存的线程数为3，允许最大的线程数为6
+        ThreadPoolExecutor tpe = new ThreadPoolExecutor(3, 6, 50, TimeUnit.MILLISECONDS, bq);
+
+        Runnable r1 = new MyThread(1);
+        Runnable r2 = new MyThread(2);
+        Runnable r3 = new MyThread(3);
+        Runnable r4 = new MyThread(4);
+
+        tpe.execute(r1);
+        tpe.execute(r2);
+        tpe.execute(r3);
+        tpe.execute(r4);
+
+        tpe.shutdown();
+    }
+
+}
---
+++ src/main/java/edu/zhanqian/thread/pool/customize/MyThread.java
@@ -0,0 +1,26 @@
+package edu.zhanqian.thread.pool.customize;
+
+/**
+ * @Date 2019/12/4 19:51
+ * @Created by zhanqian
+ * @Description TODO
+ */
+public class MyThread implements Runnable {
+
+    private int id;
+
+    public MyThread(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName() + "\t 正在被执行" + id);
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
